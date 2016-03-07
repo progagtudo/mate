6a9ad648425f5727f49a648d01b0b37080999bcf
David Mehren <dmehren1@gmail.com>
2016-03-07 20:44:02 +0100

    Remove unnecessary route

--- mate/login/login.py
+++ mate/login/login.py
@@ -18,14 +18,6 @@ def login_types(username):
     else:
         return "OMFG!", 500
 
-@app.route("/login_types/<username>")
-def login_types2(username):
-    if username in stub_login_type_holder.login_types :
-        return jsonify({"types": stub_login_type_holder.login_types[username]})
-    else:
-        print("2!")
-        return "2!", 501
-
 
 @app.route("/login/customer")
 def login_customer():
