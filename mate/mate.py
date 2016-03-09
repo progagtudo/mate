5b69036a3849776685d3299befe53d9445d6abfd
Philip Molares <philip.molares@udo.edu>
2016-03-09 19:07:33 +0100

    added runstub started with storage routes new storage object

--- mate/mate.py
+++ mate/mate.py
@@ -1,14 +1,18 @@
 from . import app, __version__
+from mate import storage
+
 
 @app.route('/')
 def hello_world():
     """Return 'Hello World!' on every Request."""
     return 'Hello World!'
 
+
 @app.route('/version')
 def version():
     """Returns API-Version"""
     return __version__
+
 
 if __name__ == '__main__':
     app.run()
