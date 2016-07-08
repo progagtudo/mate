762d4351463f8fd209e88e8fef2dbaf6153af968
Timo Walter <timo.walter@udo.edu>
2016-07-08 17:37:45 +0200

    Implemented get_retailers(), add_retailer(), update_retailer() and delete-retailer. Except the parts that require DB access

--- mate/retailer_routes.py
+++ mate/retailer_routes.py
@@ -4,17 +4,18 @@ from flask import request, jsonify
 
 from mate import app
 from mate.login.login import auth, AuthType
-from mate.model.storage.storage import Storage
+from mate.model.retailer.retailer import Retailer
 
 
-@app.route("/retailer/<int:id>/",methods=["GET"])
+@app.route("/retailer/<int:id>/", methods=["GET"])
 # ToDo: Check if Client is allowed to list retailers
 # @auth(AuthType.client)
-def get_retailer():
+def get_retailer(id):
     """
     returns the retailer
     """
     # ToDO : implement this shit
+    # ToDo: find the id in the db
 
 
 @app.route("/retailers", methods=["GET"])
@@ -24,7 +25,21 @@ def get_retailers():
     """
     returns a list of retailers
     """
-    # ToDo: implement this shit
+    limit = request.args.get("limit", 20, type=int)
+    offset = request.args.get("offset", 0, type=int)
+
+    next_link = "/retailers?limit{0}&offset={1}".format(limit, (offset + limit))
+    previous = None
+    if(offset - limit) >= 0:
+        previous = "retailers?limit={0}&offset={1}".format(limit, (offset - limit))
+    # ToDo: Generate JSON and return
+    response = {
+        "next": next_link,
+        "previous": previous,
+        "retailers": []
+    }
+
+    return jsonify(response)
 
 
 @app.route("/retailer",methods=["POST"])
@@ -34,22 +49,38 @@ def add_retailer():
     """
     add a new retailer
     """
-    # ToDo: implement this shit
+    data = request.json
+    print("test")
+    a_retailer = Retailer.from_json_new_object(json.loads(data))
+    # ToDo: create storage in DB
+    a_retailer.id = 12  # ToDo add id from DB to Object.
+    print("added stub id to retailer")  # remove print when ToDo is done
+    return jsonify(a_retailer)
+
 
 @app.route("/retailer/<int:id>/", methods=["PATCH"])
 # ToDo: Check if Client is allowed to update retailers
 # @auth(AuthType.client)
-def update_retailer():
+def update_retailer(id):
     """
     update the retailer with the id
     """
-    # ToDo: implement this shit
+    # check_storage_id(storage_id)
+    print("test")
+    data = request.data
+    # validate(data, retaile_modul.json_scheme_new_object)
+    a_retailer = Retailer.from_json_new_object(json.loads(data))
+    a_retailer.id = id
+    # ToDo: check if storage exists in DB
+    # ToDo: update Retailer
 
 
 @app.route("/retailer/<int:id>/", methods=["DELETE"])
 # ToDo: Check if Client is allowed to delete retailers
 # @auth(AuthType.client)
-def delete_retailer():
+def delete_retailer(id):
     """
     deletes the retailer with the id
     """
+    assert id is int
+    #ToDo: delete storage
