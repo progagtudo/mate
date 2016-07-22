505f458914f431fcc0fd97a86ec754982cd5aec5
Thomas Hess <thomas.hess@udo.edu>
2016-07-22 23:42:17 +0200

    Routing: Added route to login types for a person »/login_types/<username>« which returns all registered login types for a person now, instead of giving a 404. The login types are separated for customer and staff login.

--- mate/login/login_routes.py
+++ mate/login/login_routes.py
@@ -20,6 +20,26 @@ def all_login_types():
     result = db.get_all_login_types()
     return jsonify({"types": result})
 
+@app.route("/login_types/<string:username>")
+@auth(AuthType.client)
+def person_login_types(username: str):
+    """ This method searches the available login types for the given user and client"""
+
+    # Find username in DB and search for applicable login types for given user, type and client combination
+
+    # Check if the user exists
+
+    if not get_db().check_if_user_exists(username):
+        return "Could not find user!", 404
+    client_name = jwt.decode(request.headers.get(ConfigHolder.jwt_header_client), key=ConfigHolder.jwt_secret_client)[
+        'mate.tpe']
+    login_type_list_staff = get_db().get_login_types(client_name, username, True)
+    login_type_list_customer = get_db().get_login_types(client_name, username, False)
+    return jsonify({"types": {
+        "salesperson": login_type_list_staff,
+        "customer": login_type_list_customer
+    }})
+
 
 @app.route("/login_types/<string:username>/<string:user_type>")
 @auth(AuthType.client)
@@ -62,7 +82,7 @@ def login_customer(username: str):
 
         login_type_list = get_db().get_login_types(client_name=client_name, username=username, is_staff=False)
         if login_type not in login_type_list:
-            return "Login for user {} not possible".format(username)
+            return "Login for user {} not possible".format(username), 403
 
         authenticator = None
         # TODO: Refactor by using a dict for the authenticator lookup
